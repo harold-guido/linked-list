203f6491b698e6dd2e1ab0c15d50bb62b795be3a
Harold Parra <gc.harold.parra@gmail.com>
2021-01-05 15:44:44 +0000

    added append, prepend and to_s methods which work to the linked list class

---
+++ lib/linkedlist.rb
@@ -0,0 +1,49 @@
+class LinkedList
+  attr_accessor :head, :tail, :nodes
+
+  def initialize(value = nil)
+    @head = Node.new(value)
+    @tail = @head
+    @nodes = []
+  end
+
+  def append(value)
+    if @tail == @head then
+      @tail = Node.new(value)
+      @head.next_node = @tail
+    else
+      temp_tail = @tail
+      @tail = Node.new(value)
+      @nodes.push(Node.new(temp_tail.value, @tail))
+
+      if @nodes.length > 1 then
+        @head.next_node = @nodes[0]
+        @nodes[-2].next_node = @nodes[-1]
+      end
+    end
+  end
+
+  def prepend(value)
+    if @head == @tail then
+      @head = Node.new(value, @tail)
+    else
+      temp_head = @head
+      temp_nodes = @nodes
+      
+      @nodes = [@head] + temp_nodes
+      @head = Node.new(value, @nodes[0])
+
+      (@nodes.length-1).times do |i|
+        @nodes[i].next_node = @nodes[i+1]
+      end
+    end
+  end
+
+  def to_s(node = @head)
+    if node == @tail then
+      return "[ #{node.value} ] -> [ nil ]" 
+    else
+      return "[ #{node.value} ] -> #{to_s(node.next_node)}"
+    end
+  end
+end
---
+++ lib/test.rb
@@ -0,0 +1,6 @@
+require_relative 'linkedlist'
+require_relative 'node'
+
+test_list = LinkedList.new("Hello")
+
+p test_list.to_s
